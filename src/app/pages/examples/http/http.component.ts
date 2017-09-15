8331129f24972d7285a011b2fe9cdb64936490a5
Joe Jordan Brown <joe.jordan.brown@gmail.com>
2017-09-15 03:09:53 +0100

    fix cors problem

--- src/app/pages/examples/http/http.component.ts
+++ src/app/pages/examples/http/http.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { Jsonp, Response } from '@angular/http';
+import { Response } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -11,15 +11,27 @@ export class ExampleHttpComponent implements OnInit, AfterViewInit {
 
   api: any;
 
-  constructor(private _http: HttpClient, private _jsonp: Jsonp) { }
+  constructor(private _http: HttpClient) { }
 
   ngOnInit() { }
 
   ngAfterViewInit() {
-    this._jsonp.get('https://widgets.pinterest.com/v3/pidgets/boards/conradopoke/uidesign/pins/?sub=uk&base_scheme=https&callback=JSONP_CALLBACK').subscribe((response: Response) => {
+
+    // url can't be a relative link see issue https://github.com/angular/universal/issues/766
+    // const url = 'assets/data/pinterest.json';
+    const url = 'https://uixd.co.uk/open-source-software/angular-cli-universal-api/pinterest/pins.json';
+    this._http.get(url).subscribe((response: Response) => {
+      this.api = response;
+    });
+
+    // Doesn't work with server side rendering.
+    /*
+    const url = 'https://widgets.pinterest.com/v3/pidgets/boards/conradopoke/uidesign/pins/?sub=uk&base_scheme=https';
+    this._jsonp.get(url + '&callback=JSONP_CALLBACK').subscribe((response: Response) => {
       const data = response.json().data;
       console.log(data);
       this.api = data.pins;
     });
+    */
   }
 }
