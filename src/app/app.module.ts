889b082cf00c8ab9084ad3e743fb1f7574891c0d
Joe Jordan Brown <joe.jordan.brown@gmail.com>
2017-09-15 03:49:20 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {
   MdToolbarModule
 } from '@angular/material';
 
+
 // import { DragScrollModule } from 'angular2-drag-scroll/src/angular2-drag-scroll';
 
 import { DragScrollModule } from 'angular2-drag-scroll';
@@ -28,10 +29,6 @@ import { ExampleTabsComponent } from './pages/examples/tabs/tabs.component';
 import { ExampleTabRouteComponent } from './pages/examples/tabs/route/route.component';
 import { ExampleHttpComponent } from './pages/examples/http/http.component';
 import { ExampleDragScrollComponent } from './pages/examples/drag-scroll/drag-scroll.component';
-import { JsonpModule } from '@angular/http';
-
-
-
 
 
 // import 'hammerjs';
@@ -51,10 +48,8 @@ import { JsonpModule } from '@angular/http';
   imports: [
     AppRoutingModule,
     BrowserModule.withServerTransition({appId: 'angular-cli-universal-app'}),
-    // MetaModule.forRoot(),
     BrowserAnimationsModule,
     HttpClientModule,
-    JsonpModule,
     MdToolbarModule,
     MdCardModule,
     MdButtonModule,
