9e28c21e66510325badf7701ebda5d63c1d284e8
Joe Jordan Brown <joe.jordan.brown@gmail.com>
2017-09-23 18:21:13 +0100

    update packages

--- src/express.server.js
+++ src/express.server.js
@@ -1,15 +1,19 @@
-const path = require('path');
-const fs = require('fs');
-const express = require('express');
-const compression = require('compression');
-const ngExpressEngine = require('@nguniversal/express-engine').ngExpressEngine;
-const apicache = require('apicache');
-
-
 require('zone.js/dist/zone-node');
 require('rxjs/add/operator/filter');
 require('rxjs/add/operator/map');
 require('rxjs/add/operator/mergeMap');
+require('reflect-metadata');
+
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const compression = require('compression');
+const apiCache = require('apicache');
+
+const { ngExpressEngine } = require('@nguniversal/express-engine');
+const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
+
+
 
 var hash;
 fs.readdirSync(__dirname).forEach(file => {
@@ -18,34 +22,36 @@ fs.readdirSync(__dirname).forEach(file => {
   }
 });
 
-// const mailFile = fs.readdirSync(__dirname).filter(file => file.startsWith('main'));
-// const hash = mailFile[0].split('.')[1];
 
-const AppServerModuleNgFactory = require('./main.' + hash + '.bundle').AppServerModuleNgFactory;
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./main.' + hash + '.bundle');
 
 const app = express();
-const cache = apicache.middleware;
+const cache = apiCache.middleware;
 const port = Number(process.env.PORT || 8080);
-
-app.use(cache('60 minutes'));
+const baseUrl = `http://localhost:${port}`;
 
 app.engine('html', ngExpressEngine({
-  baseUrl: 'http://localhost:' + port,
-  bootstrap: AppServerModuleNgFactory
+  baseUrl: baseUrl,
+  bootstrap: AppServerModuleNgFactory,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
 }));
+
 
 
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, '/../browser'));
 
+app.use(cache('60 minutes'));
 app.use(compression());
 app.use('/', express.static(path.join(__dirname, '/../browser'), {index: false}));
 
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
   res.render('index', {
     req: req,
-    // res: res
+    res: res
   });
 });
 
